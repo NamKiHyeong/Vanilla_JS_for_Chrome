a7e1efbf55890b5e0e774542a77245cac2a2bdb3
User <User@DESKTOP-E5O0C9Q.lan>
2022-08-31 00:51:52 +0900

    Functions

--- NCjavaScript/src/main/webapp/app.js
+++ NCjavaScript/src/main/webapp/app.js
@@ -1,1 +1,10 @@
-alert('hi');
+function sayHello(nameOfPerson, age){
+	console.log('Hello my name is ' + nameOfPerson + "and I'm" + age);
+}
+
+sayHello('kihyeong ', 26);
+sayHello('dal ', 23);
+sayHello('lynn ', 21);
+
+
+
